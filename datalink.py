4b469526193a0b63f37944ce57fd3a8171d1cf2c
Cecelia Auerswald <cecelia.auerswald@gmail.com>
2015-02-12 12:28:26 -0500

    adding mac header layer start

--- datalink.py
+++ datalink.py
@@ -1,3 +1,6 @@
+addresses = ['A', 'B', 'C', 'D']
+protocols = ['NETWORK']
+
 class MacHeader():
 
 	def __init__(self, destination, source, next_protocol, payload):
@@ -9,17 +12,31 @@ class MacHeader():
 		self.next_protocol = next_protocol
 		self.payload = payload
 
+	def verify(self):
+		if not self.destination in addresses: 
+			raise ValueError("That is not a valid destination.")
+		if not self.source in addresses:
+			raise ValueError("This is not a valid source.")
+		if not self.next_protocol in protocols:
+			raise ValueError("That is not a valid next protocol code.")
+
 	def create_header(self):
+		self.verify()
 		return self.destination + self.source + self.next_protocol + self.payload + self.create_checksum()
 
 	def create_checksum(self):
 		'''
-		
+		this creates the crc 
 		'''
+		return "checksum"
 
 def encode_mac_header(mac_header_obj):
-
 	return mac_header_obj.create_header()
 
+def decode_mac_header(mac_header_text):
+	'''
+	convert the mac header string into a mac header object 
+	'''
+	return
 
 
--- morse.py
+++ morse.py
@@ -1,8 +1,12 @@
 from bj import *
 from physical import physical_up, physical_down
+from datalink import MacHeader, encode_mac_header
 
 if __name__ == "__main__":
   bj_layer = BJ(physical_down, physical_up)
   stack = BJ_Stack([bj_layer])
  
   print(stack.ascend(stack.descend("what a pal")))
+
+  mac_header_obj = MacHeader('A', 'B', "NETWORK", "HERESOMEDATA")
+  print(encode_mac_header(mac_header_obj))
