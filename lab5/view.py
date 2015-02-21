17f1017c384cd685677d6c2d427aab17bbe719dc
Philip Seger <seger.philip@gmail.com>
2015-02-20 20:51:57 -0500

    added hangman, working on words now

--- lab5/view.py
+++ lab5/view.py
@@ -1,3 +1,53 @@
+blank = ('                    \n' + 
+        '      +------+      \n' + 
+        '      |      |      \n' + 
+        '      |             \n' + 
+        '      |             \n' + 
+        '      |             \n' +
+        '    -----           \n')
+one_hit = ('                    \n' + 
+          '      +------+      \n' + 
+          '      |      |      \n' + 
+          '      |      0      \n' + 
+          '      |             \n' + 
+          '      |             \n' +
+          '    -----           \n')
+two_hit = ('                    \n' + 
+          '      +------+      \n' + 
+          '      |      |      \n' + 
+          '      |      0      \n' + 
+          '      |      |      \n' + 
+          '      |             \n' +
+          '    -----           \n')
+thr_hit = ('                    \n' + 
+          '      +------+      \n' + 
+          '      |      |      \n' + 
+          '      |      0      \n' + 
+          '      |     /|      \n' + 
+          '      |             \n' +
+          '    -----           \n')
+for_hit = ('                    \n' + 
+          '      +------+      \n' + 
+          '      |      |      \n' + 
+          '      |      0      \n' + 
+          '      |     /|\     \n' + 
+          '      |             \n' +
+          '    -----           \n')
+fiv_hit = ('                    \n' + 
+          '      +------+      \n' + 
+          '      |      |      \n' + 
+          '      |      0      \n' + 
+          '      |     /|\     \n' + 
+          '      |     /       \n' +
+          '    -----           \n')
+six_hit = ('                    \n' + 
+          '      +------+      \n' + 
+          '      |      |      \n' + 
+          '      |      0      \n' + 
+          '      |     /|\     \n' + 
+          '      |     / \     \n' +
+          '    -----           \n')
+
 class View():
   """
   Render the view of a hangman window in terminal
@@ -5,25 +55,21 @@ class View():
   def __init__(self, window, word):
     self.window = window
     self.word = word
-    self.default_screen()
+    self.redraw_screen()
+    self.draw_blanks()
     self.print_screen()
 
-  def default_screen(self):
+  def redraw_screen(self):
     # default view for hangman
-    self.window.append(
-                  '                    \n' + 
-                  '  +----+            \n' + 
-                  '  |    |            \n' + 
-                  '  |                 \n' + 
-                  '  |                 \n' + 
-                  '  |                 \n' +
-                  '-----               \n' +
-                  '                    \n' +
-                  '                    \n')
+    self.window = ''
+    self.window += blank
+
+  def draw_blanks(self):
+    self.window += ' __ '*len(self.word) + '\n'
 
   def print_screen(self):
     for i in self.window:
-      print (i)
+      print (i, end="")
 
 if __name__ == '__main__':
   View([], 'test')
