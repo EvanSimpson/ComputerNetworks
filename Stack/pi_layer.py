70fdfb66769fea5fff8109ac47a2ce28db8b94f8
Maor Bernstein <maor.bernstein@students.olin.edu>
2015-03-10 18:08:26 -0400

    Fixing Physical

--- Stack/pi_layer.py
+++ Stack/pi_layer.py
@@ -20,8 +20,8 @@ def delay(t):
 def read_pin(pin):
 	return GPIO.input(pin)
 
-def transmit(data, dtpin=17,ctpin = 18, crpin = 22, duration = .25):
-	prepare_pins(dtpin,ctpin,crpin)
+def transmit(data, dtpin=17,ctpin = 18, crpin = 22, drpin = 23, duration = .0025):
+	prepare_pins(dtpin,ctpin,crpin,drpin)
 	counter = 0
 	while(True):
 		busy = read_pin(crpin)
@@ -42,4 +42,54 @@ def transmit(data, dtpin=17,ctpin = 18, crpin = 22, duration = .25):
 	turn_low(dtpin)
 	turn_low(ctpin)
 
+def receive(duration=.0025, drpin=23):
+    prepare_pin(pin)
+    bin_string = ""
+    buffer = []
+    msg_start = False
+    zero_cnt = 0
+    while 1:
+        if read_pin(pin):
+            msg_start = True
+            buffer.append(1)
+            zero_cnt = 0
+            delay(duration)
+        elif msg_start:
+            zero_cnt+=1
+            if zero_cnt > 100:
+                msg_start = False
+                break
+            buffer.append(0)
+            delay(duration)
+    zeroes = 0
+    ones = 0
+    if buffer[0] == 1:
+        ones = 1
+    else:
+        zeroes = 1
+    for i in range(len(buffer) - 1):
+        if buffer[i+1] == buffer[i]:
+            if buffer[i+1] == 0:
+                zeroes += 1
+            else:
+                ones += 1
+        else:
+            if buffer[i+1] == 0:
+                zeroes = 1
+                if ones > 8:
+                    bin_string += "111"
+                else:
+                    bin_string += "1"
+                ones = 0
+            else:
+                ones = 1
+                if zeroes <= 8:
+                    bin_string += "0"
+                elif zeroes > 8 and zeroes <= 26:
+                    bin_string += "000"
+                else:
+                    bin_string += "0000000"
+                zeroes = 0
+    print(bin2message(bin_string))
+    return bin2message(bin_string)	
 
