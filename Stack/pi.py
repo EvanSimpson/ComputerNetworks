56211785ce4a6a5ec306aa5eab737502a53ca85e
Maor Bernstein <maor.bernstein@students.olin.edu>
2015-03-23 00:02:43 -0400

    Added yields

--- Stack/pi.py
+++ Stack/pi.py
@@ -42,54 +42,13 @@ def transmit(data, dtpin=17,ctpin = 18, crpin = 22, drpin = 23, duration = .0025
 	turn_low(dtpin)
 	turn_low(ctpin)
 
-def receive(duration=.0025, drpin=23):
-    prepare_pin(pin)
-    bin_string = ""
-    buffer = []
-    msg_start = False
-    zero_cnt = 0
-    while 1:
-        if read_pin(pin):
-            msg_start = True
-            buffer.append(1)
-            zero_cnt = 0
-            delay(duration)
-        elif msg_start:
-            zero_cnt+=1
-            if zero_cnt > 100:
-                msg_start = False
-                break
-            buffer.append(0)
-            delay(duration)
-    zeroes = 0
-    ones = 0
-    if buffer[0] == 1:
-        ones = 1
-    else:
-        zeroes = 1
-    for i in range(len(buffer) - 1):
-        if buffer[i+1] == buffer[i]:
-            if buffer[i+1] == 0:
-                zeroes += 1
-            else:
-                ones += 1
-        else:
-            if buffer[i+1] == 0:
-                zeroes = 1
-                if ones > 8:
-                    bin_string += "111"
-                else:
-                    bin_string += "1"
-                ones = 0
-            else:
-                ones = 1
-                if zeroes <= 8:
-                    bin_string += "0"
-                elif zeroes > 8 and zeroes <= 26:
-                    bin_string += "000"
-                else:
-                    bin_string += "0000000"
-                zeroes = 0
-    print(bin2message(bin_string))
-    return bin2message(bin_string)	
 
+def receive(drpin=23):
+    prepare_pin(pin)
+    times = []
+    def cbf(channel):
+        times.append(time.time())
+    GPIO.add_event_detect(chanel,GPIO.BOTH,callback = cbf)
+    if time.time > times[-1] + .0025*20:
+        yield times
+        times = []
