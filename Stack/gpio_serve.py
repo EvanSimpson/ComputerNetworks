89ed232ec3d4c5196f04b0a53933b1f659cb6705
Evan Simpson <esimpson.rescue@gmail.com>
2015-04-05 16:06:13 -0400

    Fix indent

--- Stack/gpio_serve.py
+++ Stack/gpio_serve.py
@@ -48,9 +48,9 @@ class GPIOServe(object):
                     # Check if something has come in from the GPIO and send it
                     # over to the stack if so
                     send_buffer_lock.acquire()
-                        if len(send_buffer) > 0 && self.stack_address:
-                            s.sendto(bytearray(send_buffer[0], encoding="UTF-8"), self.stack_address)
-                            del send_buffer[0]
+                    if len(send_buffer) > 0 && self.stack_address:
+                        s.sendto(bytearray(send_buffer[0], encoding="UTF-8"), self.stack_address)
+                        del send_buffer[0]
                     send_buffer_lock.release()
 
                     # Check if something has come in from the stack and send it
