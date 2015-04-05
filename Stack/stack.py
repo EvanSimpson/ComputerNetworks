67bcb58ca62ddcba1714346348372e0b1c59709e
Evan Simpson <esimpson.rescue@gmail.com>
2015-04-05 16:04:47 -0400

    Add gpio server to stack

--- Stack/stack.py
+++ Stack/stack.py
@@ -5,10 +5,11 @@ from bj import *
 from UDP import UDP, UDPHeader, encode_udp, decode_udp
 from datalink import Mac, encode_message, decode_message
 from morse import morse_down, morse_up
-from pi import transmit, receive
-import threading
 
-lock = threading.Lock()
+localhost = '127.0.0.1'
+ownport = 5000
+authorityport = 5002
+gpioport = 5003
 
 class Stack():
 
@@ -33,40 +34,36 @@ class Stack():
 		self.stack = BJ_Stack([udp_layer, mac_layer, morse_layer])
 
 	def setup_servers(self):
-		self.game_server_port = 5000
-		self.localhost = '127.0.0.1'
+		self.gpio_address = (localhost, gpioport)
+		self.stack_address = (localhost, ownport)
 
 		self.game_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		self.game_server_socket.bind((self.localhost, self.game_server_port))
+		self.game_server_socket.bind(self.stack_address)
+
+		self.gpio_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.gpio_server_socket.sendto(bytearray(0x01), self.gpio_address)
 
 	#listening out over joesocket and gpio
 
 	def listen_for_input(self):
-		gpio_thread = self.listen_over_gpio()
 		while True:
 
-			lock.acquire(blocking=True)
 			try:
-				if (received_packet!=""):
-					self.handle_input_from_gpio(received_packet)
-			finally:
-				lock.release()
-
-			self.listen_for_games()
+				self.listen_over_gpio()
+				self.listen_for_games()
+			except:
+				self.gpio_server_socket.close()
+				self.game_server_socket.close()
+				sys.exit()
 
 	def listen_over_gpio(self):
-		gpio_thread = threading.Thread(target=receive, args=[lock], name="gpio_receive_thread")
-		gpio_thread.start()
-		return gpio_thread
+		(incoming, gpio_server_address) = self.gpio_server_socket.recvfrom(1024)
+		self.handle_input_from_gpio(incoming)
+
 
 	def listen_for_games(self):
-		try:
-			(input_from_client, client_address) = self.game_server_socket.recvfrom(1024)
-			self.handle_input_from_client(input_from_client, client_address)
-		except KeyboardInterrupt:
-			# continue
-			self.game_server_socket.close()
-			sys.exit()
+		(input_from_client, client_address) = self.game_server_socket.recvfrom(1024)
+		self.handle_input_from_client(input_from_client, client_address)
 
 	#handling inputs
 
@@ -101,9 +98,9 @@ class Stack():
 		self.send_acknowledgement(self.active_game_ports[port])
 
 	def joesocket_close(self, source_address):
-		port = source_address[1] 
+		port = source_address[1]
 		if port in self.active_game_ports:
-			socket_port = self.active_game_ports.pop(port)	
+			socket_port = self.active_game_ports.pop(port)
 		self.send_acknowledgement(socket_port)
 
 	def joesocket_sendto(self, source_address, destination_address, data):
@@ -120,7 +117,7 @@ class Stack():
 		udp_obj = self.initialize_udp(source_address, destination_address, message_to_send)
 		to_transmit_string = self.stack.descend(udp_obj)
 		to_transmit = bytearray(to_transmit_string, encoding='UTF-8')
-		# self.gpio_server_socket.sendto(to_transmit, self.gpio_address)
+		self.gpio_server_socket.sendto(to_transmit, self.gpio_address)
 
 	def initialize_udp(self, source_address, destination_address, message_to_send):
 		udp_header = UDPHeader()
