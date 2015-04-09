7c8437a7524b51eb0e8c1634e21dee5148d67c69
Evan Simpson <me@evansimpson.com>
2015-04-09 04:15:40 +0000

    Send test successfull

--- Stack/transmit_test.py
+++ Stack/transmit_test.py
@@ -3,6 +3,7 @@ from bj import *
 from UDP import UDP, UDPHeader, encode_udp, decode_udp
 from datalink import Mac, encode_message, decode_message
 from morse import morse_down, morse_up
+import socket
 
 if __name__ == "__main__":
 	morse_layer = BJ(morse_down, morse_up)
@@ -10,6 +11,8 @@ if __name__ == "__main__":
 	udp_layer = BJ(encode_udp, decode_udp)
 
 	stack = BJ_Stack([udp_layer, mac_layer, morse_layer])
+
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 	def send(raw_data):
 		data = raw_data.upper()
@@ -19,7 +22,8 @@ if __name__ == "__main__":
 			udp_obj = UDP(udp_header, srcAddr='A', destAddr='C')
 			data = stack.descend(udp_obj)
 			#print(data)
-			pi.transmit(data, 18, 23)
+			sock.sendto(bytearray(data, encoding="UTF-8"), ('127.0.0.1', 5003))
+			#pi.transmit(data, 18, 23)
 	while True:
 		try:
 			s = input('--> ')
@@ -27,4 +31,5 @@ if __name__ == "__main__":
 		except KeyboardInterrupt:
 			break
 
+	sock.close()
 	pi.kill()
