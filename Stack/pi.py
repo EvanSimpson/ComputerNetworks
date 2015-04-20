53d11fe94f413a17ac9ebac17786c42f85c58dfe
Evan Simpson <esimpson.rescue@gmail.com>
2015-04-20 13:08:16 -0400

    Speed up & print statements

--- Stack/pi.py
+++ Stack/pi.py
@@ -23,7 +23,7 @@ def turn_low(pin):
 def read_pin(pin):
 	return GPIO.input(pin)
 
-def transmit(data, data_pin = 18, carrier_pin = 23, duration = 0.08, debug=False):
+def transmit(data, data_pin = 18, carrier_pin = 23, duration = 0.04, debug=False):
 	prepare_pins_in(data_pin,carrier_pin)
 	counter = 0
 	while(True):
@@ -36,6 +36,7 @@ def transmit(data, data_pin = 18, carrier_pin = 23, duration = 0.08, debug=False
 			break
 		time.sleep(duration)
 	prepare_pins_out(data_pin, carrier_pin)
+	print("turn carrier high")
 	turn_high(carrier_pin)
 	for i in range(len(data)):
 		if data[i]=="1":
@@ -45,9 +46,10 @@ def transmit(data, data_pin = 18, carrier_pin = 23, duration = 0.08, debug=False
 		time.sleep(duration)
 	turn_low(data_pin)
 	time.sleep(duration)
+	print("turn carrier low")
 	turn_low(carrier_pin)
 
-def receive(lock, recv_flag, data_pin=18, carrier_pin = 23, duration=0.08, debug=False):
+def receive(lock, recv_flag, data_pin=18, carrier_pin = 23, duration=0.04, debug=False):
 	prepare_pins_in(data_pin, carrier_pin)
 	times = []
 	def data_callback(channel):
